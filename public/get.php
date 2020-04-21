b85038f350181cba5e8118acc94e5eefb83aca9d
Dylan <dylanelliott27@hotmail.com>
2020-04-21 11:09:13 -0400

    heroku fix

--- public/get.php
+++ public/get.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Headers: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 if (file_exists('./.env.php')) {
     require_once('./.env.php');
--- public/post.php
+++ public/post.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 if (file_exists('./.env.php')) {
     require_once('./.env.php');

0ea6ec171c3626b02fcedaae85d0bef0c61fa2c9
Dylan <dylanelliott27@hotmail.com>
2020-04-23 12:13:01 -0400

    conversion to nodejs backend

--- client/src/Create.js
+++ client/src/Create.js
@@ -29,7 +29,7 @@ function Create(props) {
         const form = document.getElementById('recordform');
         var forminfo = new FormData(form);
 
-        fetch('https://diabeteslogger.herokuapp.com/public/post.php', {body: forminfo, method: 'post'})
+        fetch('/api/insert', {body: forminfo, method: 'post'})
         .then(res =>{
             if(res.status === 200){
                 console.log("SUCCESS");
